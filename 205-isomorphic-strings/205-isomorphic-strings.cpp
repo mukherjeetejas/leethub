0bf89118bf17f45965192dba6dc46e958f3bec25
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2022-07-11 20:00:55 -0700

    Time: 9 ms (62.45%), Space: 7 MB (75.25%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size()!=t.size()) return false;
+        
+        const int MAXCHAR = 256;
+        char maps[MAXCHAR]={0}, mapt[MAXCHAR]={0};
+        //memset(maps, 0, sizeof(maps));
+        //memset(mapt, 0, sizeof(mapt));
+        
+        for(int i=0; i<s.size(); i++){
+            if(maps[s[i]] == 0 && mapt[t[i]] == 0){
+                maps[s[i]] = t[i];
+                mapt[t[i]] = s[i];
+                continue;
+            }
+            if(maps[s[i]] == t[i] && mapt[t[i]] == s[i]) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+};
