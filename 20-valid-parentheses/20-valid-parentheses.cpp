f4973b0c939836a11ec1115c7dd0863dfe81c195
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2022-09-14 01:56:54 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (10.27%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -5,19 +5,23 @@ public:
             case '}': return '{'; break;
             case ']': return '['; break;
             case ')': return '('; break;
+            //must return something in all control paths
             default: return 'e';
         }
     }
     bool isValid(string s) {
+        //stack
         stack<int> stk;
         for (int i=0;i<s.size();i++) {
             if (s[i]=='(' or s[i]=='{' or s[i]=='[') 
                 stk.push(s[i]);
             else {
+                //if stack doesnt exists then top() will give error
                 if (stk.size() and getClosing(s[i])==stk.top()) stk.pop();
                 else return false;
             }
         }
+        //final stack must be empty
         if (!stk.size()) return true;
         return false;
     }
